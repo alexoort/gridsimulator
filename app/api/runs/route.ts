780e747c1a2f22a1cf9d78400ff3788e977658fb
alexoort <aoortalonso@college.harvard.edu>
2025-01-17 19:29:48 -0500

    deleted route.ts

--- app/api/runs/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  return NextResponse.json({ message: 'Hello from the API' })
-}
-
-export async function POST(request: Request) {
-  return NextResponse.json({ message: 'POST request received' })
-} 
