1f13c52498c97ec98784df2b37a68b372e463fa3
alexoort <aoortalonso@college.harvard.edu>
2025-01-20 15:22:09 +0000

    CO2 emissions + home

--- app/api/auth/init-db.ts
+++ app/api/auth/init-db.ts
@@ -13,6 +13,18 @@ export async function initializeDatabase() {
         high_score INTEGER DEFAULT 0
       );
     `;
+    await sql`
+      CREATE TABLE IF NOT EXISTS runs (
+        id SERIAL PRIMARY KEY,
+        user_id INTEGER REFERENCES users(id),
+        start_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        end_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        money_made DECIMAL,
+        max_frequency DECIMAL,
+        min_frequency DECIMAL,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+      );
+    `;
     console.log('Database initialized successfully');
   } catch (error) {
     console.error('Failed to initialize database:', error);
